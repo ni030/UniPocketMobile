54be2fecded7ac604f6263315500c1eb9bf1a31f
LIM SI NI <limsini000@gmail.com>
2025-03-06 18:04:34 +0800

    enhancedFrontend

--- frontend/app/reportFacilities/reportFeedback.tsx
+++ frontend/app/reportFacilities/reportFeedback.tsx
@@ -1,16 +1,58 @@
 import React, { useState } from 'react'
-import { ScrollView, Text, TouchableOpacity, View } from 'react-native'
+import { Modal, Pressable, ScrollView, Text, TouchableOpacity, View } from 'react-native'
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
 import Ionicons from '@expo/vector-icons/Ionicons';
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+import AntDesign from '@expo/vector-icons/AntDesign';
 
 const ReportFeedback = () => {
-    const [personalDetailsExpanded, setPersonalDetailsExpanded] = useState(true);
-    const [reportDetailsExpanded, setReportDetailsExpanded] = useState(true);
-    const [cancelAvaliable, setCancelAvaliable] = useState(false);
-    const [completeAvaliable, setCompleteAvaliable] = useState(true);
+    const [personalDetailsExpanded, setPersonalDetailsExpanded] = useState<boolean>(true);
+    const [reportDetailsExpanded, setReportDetailsExpanded] = useState<boolean>(true);
+    const [cancelAvaliable, setCancelAvaliable] = useState<boolean>(false);
+    const [completeAvaliable, setCompleteAvaliable] = useState<boolean>(true);
+    const [modalRatingVisible, setModalRatingVisible] = useState<boolean>(false);
+    const [rating, setRating] = useState<number>(0);
+    
+    const renderStars = (rating: number) => {
+        const stars = [];
+        for (let i = 0; i < 5; i++) {
+            if (i < rating) {
+                stars.push(<Pressable onPress={()=>(setRating(i+1))}><AntDesign key={i} name="star" size={28} color="#9f1239" /></Pressable>);
+            } else {
+                stars.push(<Pressable onPress={()=>(setRating(i+1))}><AntDesign key={i} name="staro" size={28} color="#9f1239" /></Pressable>);
+            }
+        }
+        return stars;
+    }
+
   return (
     <View className='w-full h-full bg-gray-100 flex justify-start items-center p-4'>
+        <Modal
+            animationType="fade"
+            transparent={true}
+            visible={modalRatingVisible}
+            onRequestClose={() => {
+                setModalRatingVisible(!modalRatingVisible);
+            }}>
+                <View className='h-full w-full flex justify-center items-center bg-gray-100/70'>
+                <View className='h-auto w-4/5 flex justify-center items-start gap-3 bg-white rounded-lg p-8'>
+                    <View className='f-1/6 w-full p-2 flex flex-row justify-between items-center'>
+                    <Text className='text-2xl font-semibold'>Rating</Text>
+                    <Pressable
+                        onPress={() => setModalRatingVisible(!modalRatingVisible)}>
+                        <FontAwesome6 name="xmark" size={18} color="black" />
+                    </Pressable>
+                    </View>
+                    <View className='h-auto w-full p-3 flex flex-row justify-center items-center gap-4'>
+                        {renderStars(rating)}
+                    </View>
+                    <TouchableOpacity className='h-auto w-full bg-rose-800 rounded-lg p-3'>
+                        <Text className='text-white text-center font-bold'>Submit</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </Modal>
         <View className='w-full h-1/5 bg-white rounded-xl flex justify-start items-center p-4 border-2 border-gray-200 my-3'>
             <Text className='font-medium text-xl text-center m-3'>Report Tracking</Text>
             <View className='w-4/5 h-auto flex flex-row justify-center my-6 items-center gap-2'>
@@ -81,7 +123,7 @@ const ReportFeedback = () => {
                     <TouchableOpacity className={`h-auto w-1/2 rounded-lg ${cancelAvaliable ? 'bg-rose-800' : 'bg-gray-300'}`}>
                         <Text className='text-white p-3 font-bold text-center'>Cancel</Text>
                     </TouchableOpacity>
-                    <TouchableOpacity className={`h-auto w-1/2 rounded-lg ${completeAvaliable ? 'bg-rose-800' : 'bg-gray-300'}`}>                        
+                    <TouchableOpacity className={`h-auto w-1/2 rounded-lg ${completeAvaliable ? 'bg-rose-800' : 'bg-gray-300'}`} onPress={() => setModalRatingVisible(true)}>                       
                     <Text className='text-white p-3 font-bold text-center'>Complete</Text>
                     </TouchableOpacity>
 
