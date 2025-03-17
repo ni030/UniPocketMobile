d7d946a932728a3b09ceef794cb50b425ca04cb5
LIM SI NI <130362958+ni030@users.noreply.github.com>
2025-03-17 13:55:14 +0800

    Delete frontend/env.d.ts

--- frontend/env.d.ts
+++
@@ -1,7 +0,0 @@
-// @ts-ignore
-/// <reference types="nativewind/types" />
-
-declare module '@env' {
-  export const FIREBASE_API='https://us-central1-uniresidence-e1123.cloudfunctions.net/api';
-  
-}
